8e5381948196e15d357bd94ecca486dd2ddf9eb1
Santiago de Pedro <santiago.pedro23@gmail.com>
2017-03-22 17:51:29 -0400

    Multiple changes: * Adding travis ci file * Implementing function handler

--- src/rpi_web_service/handler.clj
+++ src/rpi_web_service/handler.clj
@@ -1,11 +1,50 @@
 (ns rpi-web-service.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [clojure.string :refer [upper-case]]
+            [ring.util.response :refer [response]]
+            [ring.middleware.json :refer [wrap-json-response wrap-json-body]]))
+
+;-------------------------------------------------------
+; VARS
+;-------------------------------------------------------
+
+(def available-commands [:power :freeze])
+(def not-found-route (route/not-found (response {:message "Not found"})))
+
+;-------------------------------------------------------
+; HANDLER OPTIONS
+;-------------------------------------------------------
+
+(defn- map-values
+  [m keys f & args]
+  (reduce #(apply update-in %1 [%2] f args) m keys))
+
+(defn upper-case-response
+  [response]
+  (map-values response [:command :option :status] upper-case))
+
+(defn commander
+  [command option]
+  "ok")
+
+;-------------------------------------------------------
+; WEB HANDLERS
+;-------------------------------------------------------
+(defn check-availability [] "RPI Device")
+
+(defn command-handler
+  [command option]
+  (if (some #(= (keyword command) %1) available-commands)
+    (let [response-body {:command command
+                         :option  option
+                         :status  (commander command option)}]
+      (-> response-body upper-case-response response))
+    not-found-route))
 
 (defroutes app-routes
-  (GET "/" [] "RPI Device")
-  (route/not-found "Not Found"))
+           (GET "/" [] (check-availability))
+           (PUT "/:command/:option" [command option] (command-handler command option))
+           not-found-route)
 
-(def app
-  (wrap-defaults app-routes site-defaults))
+(def app (-> app-routes wrap-json-response wrap-json-body))
--- test/rpi_web_service/handler_test.clj
+++ test/rpi_web_service/handler_test.clj
@@ -1,14 +1,51 @@
 (ns rpi-web-service.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
-            [rpi-web-service.handler :refer :all]))
+            [rpi-web-service.handler :refer :all]
+            [cheshire.core :refer :all]))
+
+(def response-code-expected 200)
+(def content-type-expected "application/json")
 
 (deftest test-app
-  (testing "main route"
-    (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "RPI Device"))))
+  (testing "Check availability test"
+    (let [response (app (mock/request :get "/"))
+          body-expected "RPI Device"]
+      (is (= (:status response) response-code-expected))
+      (is (= (:body response) body-expected))))
 
-  (testing "not-found route"
+  (testing "Power on projector test"
+    (let [response (app (mock/request :put "/power/on"))
+          body-expected {:command "POWER" :option "ON" :status "OK"}]
+      (is (= (:status response) response-code-expected))
+      (is (.contains (get-in response [:headers "Content-Type"]) content-type-expected))
+      (is (= (parse-string (:body response) true) body-expected))))
+
+  (testing "Power off projector test"
+    (let [response (app (mock/request :put "/power/off"))
+          body-expected {:command "POWER" :option "OFF" :status "OK"}]
+      (is (= (:status response) response-code-expected))
+      (is (.contains (get-in response [:headers "Content-Type"]) content-type-expected))
+      (is (= (parse-string (:body response) true) body-expected))))
+
+  (testing "Freeze on projector test"
+    (let [response (app (mock/request :put "/freeze/on"))
+          body-expected {:command "FREEZE" :option "ON" :status "OK"}]
+      (is (= (:status response) response-code-expected))
+      (is (.contains (get-in response [:headers "Content-Type"]) content-type-expected))
+      (is (= (parse-string (:body response) true) body-expected))))
+
+  (testing "Freeze off projector test"
+    (let [response (app (mock/request :put "/freeze/off"))
+          body-expected {:command "FREEZE" :option "OFF" :status "OK"}]
+      (is (= (:status response) response-code-expected))
+      (is (.contains (get-in response [:headers "Content-Type"]) content-type-expected))
+      (is (= (parse-string (:body response) true) body-expected))))
+
+  (testing "Command not found route test"
+    (let [response (app (mock/request :put "/invalid-command/on"))]
+      (is (= (:status response) 404))))
+
+  (testing "Not found route test"
     (let [response (app (mock/request :get "/invalid"))]
       (is (= (:status response) 404)))))
